8aa00026f883f8dd3f7fcb0ad5579fc0f90aa5f3
GlnVDu <egor.deynega@bk.ru>
2023-11-20 06:03:30 +0300

    dffdf

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -10,7 +10,7 @@ namespace TableTest1.Data
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             base.OnConfiguring(optionsBuilder) ;
-            optionsBuilder.UseSqlServer("Server=(localdb);Database=Bible;Trusted_Connection=True;");
+            optionsBuilder.UseSqlServer("Server=DESKTOP-RN7F3DC;Database=Bible;TrustServerCertificate=True;");
         }
     }
 }
